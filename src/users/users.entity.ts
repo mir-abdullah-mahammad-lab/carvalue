673d70175698a262aa2b18e2b919e84440ec9159
mir-abdullah-mahammad-lab <mirabdullahmahammad@gmail.com>
2026-07-18 20:54:32 +0600

    CRUD-User done

---
+++ src/users/users.entity.ts
@@ -0,0 +1,31 @@
+import { AfterInsert, AfterRemove, AfterUpdate,
+    Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column()
+    email!: string;
+
+    
+    @Column()
+    password!: string;
+
+    @AfterInsert()
+    logInsert(){
+        console.log('Inserted User with ', this.id)
+    }
+
+    @AfterUpdate()
+    logUpdate(){
+        console.log('Update User with id', this.id)
+    }
+    @AfterRemove()
+    logRemove(){
+        console.log('Removed user with id', this.id)
+    }
+
+}
